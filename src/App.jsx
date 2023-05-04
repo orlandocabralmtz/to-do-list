df8ad715f5c75e3e37b1e0c6824c3781325aa9e4
Orlando Cabral <orlandocabral@live.com>
2023-05-04 11:15:11 -0500

    primer commit. iniciando el ejercicio

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import ToDoList from './components/ToDoList'
+
+function App () {
+  return (
+    <>
+      <ToDoList />  {/* se importa el componente ToDoLis */}
+    </>
+  )
+}
+
+export default App
---
+++ src/components/ToDoList.jsx
@@ -0,0 +1,8 @@
+const ToDoList = () => {
+  return (
+    <div>
+      <h1>Lista de trareas </h1>
+    </div>
+  )
+}
+export default ToDoList
