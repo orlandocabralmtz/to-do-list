d966369a72ba9d03c8fd29d3879f220a25e0eeba
Orlando Cabral <orlandocabral@live.com>
2023-05-06 19:15:29 -0500

    se termina la aplicación para crear la lista de tareas

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,14 +1,28 @@
 import { useState } from 'react'
+import ToDoItem from './ToDoItem' // se importa el componente ToDoItem
 
 const ToDoList = () => {
   // 1: los estados de React sirven para guardar información que se va a utilizar en el componente. esta informacion tiene la particularidad de que se actualiza la vista automaticamente cuando cambia el estado
   // El primer elemento del arreglo es el valor del estado y el segundo elemento es una funcion que se utiliza para actualizar el valor del estado
   const [inputValue, setInputValue] = useState('') // 2: se inicializa el estado con un string vacio - Esto se usara para un input
 
+  // se añade un estado para almamcenar la lista de tareas
+  const [todos, setTodos] = useState([]) // 5: se inicializa el estado con un arreglo vacio - Esto se usara para almacenar la lista de tareas
+
   // esta funcion se ejecuta cuando el usuario hace click en el boton agregar
+
+  // se va a modificar la funcion para que el ToDo se agrege a la lista de tareas con el arreglo de tareas
   const handleAdd = () => {
-    console.log('agregar: ', inputValue)
+    if (inputValue.trim()) {
+      setTodos([...todos, inputValue]) // 6: se agrega el valor del input al arreglo de tareas- con el spread operator se agrega el valor del input al arreglo de tareas
+      setInputValue('') // 7: se limpia el input para que el usuario pueda agregar otra tarea
+    }
   }
+  // funcion para eliminar una tarea
+  const handleDelete = (index) => { // el index es el indice del elemento que se va a eliminar
+    setTodos(todos.filter((_, i) => i !== index)) // i es el indice del elemento que se esta iterando. el _ se usa para indicar que no se va a usar el elemento
+  }
+
   return (
     <div>
       <h1>Lista de trareas </h1>
@@ -17,15 +31,14 @@ const ToDoList = () => {
 
       <button onClick={handleAdd}> Agregar</button>  {/* 4: se agrega un boton para agregar tareas */}
       <ul>
-        <li> Comprar Leche
-          <button> Eliminar</button>
-        </li>
-        <li> Comprar Pan
-          <button> Eliminar</button>
-        </li>
-        <li> Comprar Huevos
-          <button> Eliminar</button>
-        </li>
+        {/* 8: se itera el arreglo de tareas para mostrar cada tarea. por cada elemento creamos un ToDoItem */}
+        {todos.map((todo, index) => ( // el map recibe una funcion que se ejecuta por cada elemento del arreglo. el primer parametro es el elemento y el segundo parametro es el indice del elemento
+          <ToDoItem
+            todo={todo}
+            handleDelete={() => handleDelete(index)}
+            key={index} // el key es un identificador unico para cada elemento. en este caso se usa el indice del elemento. Se usa el indice porque no se va a modificar el arreglo de tareas
+          />
+        ))}
       </ul>
     </div>
   )
